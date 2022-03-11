2f3a1d622bea5262f8a25b042c4c2a0a4645c5c9
Kaguya <falin.original@gmail.com>
2022-03-11 21:52:16 +0300

    Add base methods for Stack class

--- src/org/kaguya/Stack.java
+++ src/org/kaguya/Stack.java
@@ -1,15 +1,57 @@
 package org.kaguya;
 
-public class Stack {
-    public Stack() {
+import java.lang.reflect.Array;
 
-    }
-
-    public static void peek() {
-
-    }
+public class Stack<E> {
+    private int top;
+    final private int size;
+    private E[] array;
 
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        try {
+            stack.push(3);
+            stack.push(4);
+            System.out.println(stack.peek());
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+//            System.out.println(stack.pop());
+        } catch (Exception e) {
+            System.exit(-1);
+        }
 
+    }
+
+    public Stack() {
+        this.size = 64;
+        this.top = -1;
+        array = (E[]) Array.newInstance(Object.class, this.size);
+//        array = (E[]) new Object[this.size];
+    }
+
+    public E peek() {
+        return array[this.top];
+    }
+
+    public E pop() {
+        this.top--;
+        return array[top + 1];
+    }
+
+    public boolean isEmpty() {
+        return (this.top == -1);
+    }
+
+    private boolean isFull() {
+        return (this.top + 1 == size);
+    }
+
+    public void push(E elem) throws Exception {
+        if (this.isFull())
+            throw new Exception("Stack overflow");
+        else {
+            this.top++;
+            array[top] = elem;
+        }
     }
 }
